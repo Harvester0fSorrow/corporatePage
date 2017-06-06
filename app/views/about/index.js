8d15c5a234a9594a2016adb65530ecf9e838353e
Radoslaw Lisowski <Rad@Radoslaws-MacBook-Pro.local>
2017-06-06 22:42:03 +0100

    updated homes

--- app/views/about/index.js
+++ app/views/about/index.js
@@ -1,38 +1,25 @@
-import React, { PropTypes, Component } from "react";
-import { connect } from 'react-redux';
+import React from "react";
 import Header from '../helpers/header';
-import Content from '../helpers/content';
-import MessageBox from '../helpers/messageBox'
-import * as aboutActions from '../../actions/about';
 import Footer from '../helpers/footer';
 import Messenger from '../helpers/messenger';
-import { selector } from '../../selectors/about'
 
-class About extends Component {
-    constructor(props) {
-        super(props);
-        const { dispatch } = this.props;
-        dispatch(aboutActions.getAboutContent());
-    }
-
-    render() {
-        const { about } = this.props;
-        return (
-            <div className="body">
-              <Header />
-                <div className="content">
-                  <Content content={about.content}/>
-                  <Messenger />
-                </div>
-              <Footer />
+const About = () => {
+    return (
+        <div className="body">
+          <Header />
+            <div className="content">
+              <div className="box">
+                <img className="about-image" src="http://corporatepage.blob.core.windows.net/blob/the-big-lisowski.jpg" alt="dude-glasses"/>
+                <p>Hi! My name is Radosław Lisowski and I am a full stack developer. Don't sweat to much trying to pronounce my name, I know it's hard for an average English speaker. Just call me Rad like everybody else.</p>
+                <p>I always had a passion for technology, so I have started studying Computer Science at Wroclaw University of Technology. After graduating I wanted to work with networks and system administration, but I quickly realised that I am much more interested in software development. Because of that I dusted of my programing books and started pursuing that exact career.</p>
+                <p>I enjoy researching new technologies to stay up to date, and deliver the best software and user experience possible. I am mostly working with .NET Framework, Azure and React.js but I also worked with Java and Objective C. Lately I am working in a startup that helps companies to understand their customer base.</p>
+                <p>Privately I''m an adrenaline junkie and I enjoy downhill, mountain biking, climbing and other extreme sports and activities. I own a husky named Serj, so I have to stay in shape to keep up with him during our daily walks or rather runs. As you can see I spend a lot of my free time outside "enjoying" Scottish weather. And yes, you guessed it! My general health and immunity is excellent!</p>
+              </div>
+              <Messenger />
             </div>
-        );
-    }
+          <Footer />
+        </div>
+    );
 }
 
-About.propTypes = {
-    about: PropTypes.object,
-    dispatch: PropTypes.func
-}
-
-export default connect(selector)(About)
+export default About
